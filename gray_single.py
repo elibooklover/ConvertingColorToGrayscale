8db082d172c6ab4fec715828c2211b6002a5ba5a
Hoyeol Kim <elibooklover@gmail.com>
2019-11-23 15:03:56 -0600

    First Commit

---
+++ gray_single.py
@@ -0,0 +1,13 @@
+import cv2
+
+directory1 = r'resized/'
+directory2 = r'gray/'
+
+image = cv2.imread(directory1+'image1.png')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imwrite(directory2+'image1_gray.png', gray)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
